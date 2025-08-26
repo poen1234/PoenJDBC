9a608b3cca17af8653e9f0d967c8ce32f03ed86a
user1 <user1@DESKTOP-U4LFSEV>
2025-08-26 12:12:09 +0900

    [MODIFY]회원 정보 수정시 데이터 확인 여부
    회원정보 수정시 아이디 입력을 하고 해당 아이디의 객체가
    존재하지는 확인하는 코드를 추가하였음.

--- src/com/poen/jdbc/day02/stmt/member/view/MemberView.java
+++ src/com/poen/jdbc/day02/stmt/member/view/MemberView.java
@@ -43,15 +43,20 @@ public class MemberView {
 			case 4 : 
 				// 아이디 입력 받기
 				memberId = inputMemberId();
-				// 수정할 정보 입력받기
-				member = modifyMember(memberId);
-				// 디비에서 정보 수정하도록 요청하기
-				result = mController.updateMember(member);
-				// 결과에 따른 메시지 출력하기
-				if(result > 0) {
-					printMessage("회원 정보 수정이 완료되었습니다.");
+				member = mController.findOneById(memberId);
+				if(member != null) {
+					// 수정할 정보 입력받기
+					member = modifyMember(memberId);
+					// 디비에서 정보 수정하도록 요청하기
+					result = mController.updateMember(member);
+					// 결과에 따른 메시지 출력하기
+					if(result > 0) {
+						printMessage("회원 정보 수정이 완료되었습니다.");
+					}else {
+						printMessage("회원 정보가 수정되지 않았습니다.");
+				}
 				}else {
-					printMessage("회원 정보가 수정되지 않았습니다.");
+					System.out.println("해당 정보가 존재하지 않습니다.");
 				}
 				break;
 			case 5 : 
